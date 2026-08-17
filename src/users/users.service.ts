77fc29bb68b50dd977e91e93caf8fd4cb8270c3a
Pedro Acioly <pedrocastroacioly27@gmail.com>
2026-08-17 16:21:10 -0300

    feat: secure user password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -12,13 +13,20 @@ export class UsersService {
     private readonly userRepository: Repository<User>,
   ) {}
   async create(createUserDto: CreateUserDto) {
-    const findUser = await this.userRepository.findOne({
+    const userExists = await this.userRepository.findOne({
       where: { email: createUserDto.email },
     });
-    if (findUser) {
+    if (userExists) {
       throw new BadRequestException('email already in use');
     }
-    const user = this.userRepository.create(createUserDto);
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    const user = this.userRepository.create({
+      name: createUserDto.name,
+      email: createUserDto.email,
+      password: hashedPassword,
+    });
 
     const { password: _, ...newUser } = user;
 
